2509d97005f8084864b45e57188ed4ec6e7b8942
Pierre <pierre.mallevaey@laposte.net>
2017-06-04 19:25:56 +0200

    ajout du click grille et btn

--- client/src/fenetrePosBateau.cpp
+++ client/src/fenetrePosBateau.cpp
@@ -9,23 +9,23 @@ int fenetrePosBateau()
 
 ////////////////// creation et init de la fenetre //////////////////
 
-	RenderWindow window(sf::VideoMode(1000, 800), "Peaceful Shadow Online : Positionne tes bateaux Matelot !!",Style::Close);
+	RenderWindow window(sf::VideoMode(1000, 800), "Peaceful Shadow Online : Positionne tes bateaux Matelot !!",Style::None);
 	window.setPosition(Vector2i(10, 10));
 
 
 ////////////////// creation et init des variables //////////////////
 
 	string pseudoEntr_s = "";
+	int cpt = 5;
+	int btx , bty;
 	Event event;
 
 
 ////////////////// chargement des fonts et textures /////////////////
 
 	Texture fondEcran_tex;
-	if (!fondEcran_tex.loadFromFile("../img/accueil.png")){
+	if (!fondEcran_tex.loadFromFile("../img/bateau.png")){
 	}
-	Font main_font;
-	main_font.loadFromFile("../fonts/main_font.ttf");
 	
 
 //////////// creation des textes, sprites et textures //////////////
@@ -44,24 +44,37 @@ int fenetrePosBateau()
 			
 			switch (event.type){
 				
-				case Event::Closed :
-					window.close();
-					break;
-				
-				case Event::TextEntered :
-					pseudoEntr_s.push_back((char)event.text.unicode);
-						
-					break;
-
 				case Event::KeyPressed : 
 					if (pseudoEntr_s.size() > 0 && event.key.code == Keyboard::BackSpace )
 						pseudoEntr_s.erase();
 					break;
 				
-				case Event::MouseButtonPressed :
+				case Event::MouseButtonPressed : // gestion de click dans la grille
 					if(	event.mouseButton.x < 700 && event.mouseButton.x >300 && 
-						event.mouseButton.y < 700 && event.mouseButton.y > 628)		
+						event.mouseButton.y < 700 && event.mouseButton.y > 300 &&
+						cpt >= 5){		
+						std::cout << "mouse x: " << event.mouseButton.x << std::endl;
+						std::cout << "mouse y: " << event.mouseButton.y << std::endl;
+						btx = event.mouseButton.x;
+						bty = event.mouseButton.y;
+						std::cout << "btx: " << btx << std::endl;
+						std::cout << "bty: " << bty <<std::endl;
+					}
+					
+					//gestion du click sur le bouton
+					if(	event.mouseButton.x < 700 && event.mouseButton.x >300 && 
+						event.mouseButton.y < 791 && event.mouseButton.y > 713 &&
+						cpt >= 5){		
 						std::cout << "the right button was pressed" << std::endl;
+						std::cout << "mouse x: " << event.mouseButton.x << std::endl;
+						std::cout << "mouse y: " << event.mouseButton.y << std::endl;
+						btx = event.mouseButton.x;
+						bty = event.mouseButton.y;
+						std::cout << "btx: " << btx << std::endl;
+						std::cout << "bty: " << bty <<std::endl;
+					}
+					
+					
 					break;
 						
 				default:
@@ -71,20 +84,20 @@ int fenetrePosBateau()
 
 
 //////////// creation du texte avec le pseudo //////////////////////
-
+/*
 	Text pseudoEntr_txt;
 	pseudoEntr_txt.setFont(main_font);
 	pseudoEntr_txt.setCharacterSize(35);	
 	pseudoEntr_txt.setPosition(370,376);
 	//pseudoEntr_txt.setFillColor(Color::Red);
 	pseudoEntr_txt.setString(pseudoEntr_s);
-
+*/
 ///////////// gestion de l'affichage //////////////////
 	
 	window.clear(Color::White); 	
 		
 	window.draw(fondEcran_spr);
-	window.draw(pseudoEntr_txt);
+	//window.draw(pseudoEntr_txt);
         
 	window.display();
         
