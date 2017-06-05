d6cc3dab12d5a2c6f165f3af6a1b05bfb14db99f
Pierre <pierre.mallevaey@laposte.net>
2017-06-05 16:16:43 +0200

    ajout bouton valider bateau, optimisation de la recuperation des x et y et deport de code

--- client/src/gestionGrille.cpp
+++ client/src/gestionGrille.cpp
@@ -1,13 +1,23 @@
 #include "gestionGrille.hpp"
 
-int retourX(int mouseX){
-	return mouseX/40;
+// retour X et Y pour une grille centrale
+int gdRetourX(int mouseX){
+	return (mouseX- 300)/40;
 }
 
-int retourY(int mouseY){
-	return mouseY/40;
+int gdRetourY(int mouseY){
+	return (mouseY- 300)/40;
 }
 
-void posBateau(int numeroBateau,int x, int y){
-
+// retour X et Y pour 2 grilles laterales
+int ptRetourX(int mouseX){
+	return (mouseX- 533)/40;
 }
+
+int ptRetourY(int mouseY){
+	return (mouseY- 200)/40;
+}
+
+//void posBateau(int numeroBateau,int x, int y){
+
+//}
